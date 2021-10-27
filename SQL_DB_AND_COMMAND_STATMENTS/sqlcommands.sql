3e450b7938c2987c05338b9d94be8314df0f3a0e
Ely Lam <eydelamb98@gmail.com>
2021-10-26 17:49:39 -0700

    Update sqlcommands.sql
    syntax fixes; deleted commented line

--- SQL_DB_AND_COMMAND_STATMENTS/sqlcommands.sql
+++ SQL_DB_AND_COMMAND_STATMENTS/sqlcommands.sql
@@ -5,7 +5,7 @@ JOIN Listing ON Listing.listingID = Rating.listingID
 JOIN Rating ON Rating.raterID = Student.username
 JOIN Owner ON Listing.authorID = Owner.authorID
 WHERE Student.username="bsacaze2c"
-GROUP BY offer Having offer < Student.budget
+GROUP BY offer Having offer < Student.budget;
 
 SELECT *
 FROM Owner
@@ -16,16 +16,15 @@ SELECT Listing.*,Student.username, Owner.username FROM Listing
 JOIN Student
 JOIN Owner ON Owner.authorID= Listing.authorID
 WHERE Student.username='Bernard'
-GROUP BY offer HAVING Listing.offer <= Student.budget
+GROUP BY offer HAVING Listing.offer <= Student.budget;
 
 SELECT * FROM Listing
 JOIN Owner ON Owner.authorID= Listing.authorID
-WHERE Listing.description like '/dev/null; touch /tmp/blns.fail ; echo' AND (Listing.leaseInMonths > 6 AND Listing.leaseInMonths < 10)
+WHERE Listing.description like '/dev/null; touch /tmp/blns.fail ; echo' AND (Listing.leaseInMonths > 6 AND Listing.leaseInMonths < 10);
 
 SELECT message.sender, message.receiver, Student.firstName, Student.lastName, Message.time, Message.message FROM message
 JOIN Student ON Student.username=Message.sender
--- JOIN Owner ON Owner.username=Message.receiver
-WHERE Message.sender='mblackshawo' AND message.message Like '%Duis%'
+WHERE Message.sender='mblackshawo' AND message.message Like '%Duis%';
 
 SELECT  *,
 CASE WHEN Listing.leaseInMonths > 10 THEN 'The lease is longer than one school year'
@@ -33,4 +32,4 @@ WHEN leaseInMonths > 5 THEN 'The lease is longer than one semester'
 WHEN leaseInMonths IS NULL THEN ''
 ELSE 'The lease is shorter than one semester'
 END AS 'Lease Term'
-FROM Listing
+FROM Listing;
