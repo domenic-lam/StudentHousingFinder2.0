a5875cc5f72938694523e0bc54a7f7273d0435b2
bekezie <bekezie@gmail.com>
2021-10-30 04:35:29 -0700

    express genorator

--- db/mySQLiteDB.js
+++ db/mySQLiteDB.js
@@ -1,11 +1,35 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
-
-sqlite3.verbose();
-
 async function connect() {
   return open({
     filename: "./db/studenthousing.db",
     driver: sqlite3.Database,
   });
 }
+sqlite3.verbose();
+const StudentHousingDBController = function () {
+  const studenthousingDB = {};
+
+  /*
+   ***************USER CRUD OPERATIONS*********************
+   */
+
+  //this function will save a new user to the database
+
+  studenthousingDB.saveNewUser = async function (newUser) {
+    const db = await connect();
+
+    const stmt = await db.prepare(`INSERT INTO
+      users(username, password)
+      VALUES (:username, :password)
+    `);
+    stmt.bind({
+      ":username": newUser.usename,
+      ":password": newUser.password,
+    });
+    return await stmt.run();
+  };
+
+  return studenthousingDB;
+};
+module.exports = StudentHousingDBController();
